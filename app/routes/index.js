abe72e7aa35ac642d2b1cc408d5e564b43a6bfd8
rouz majlessi <rmajlessi@gmail.com>
2016-10-29 14:28:52 -0700

    add json file in index js temporary too see if application is working

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  content: "Sports",
+  author: "Bob Costas",
+  notes: "MLB"
+  }, {
+  id: 2,
+  content: "Politics",
+  author: "Noam Chomsky",
+  notes: "Election"
+  }, {
+  id: 3,
+  content: "Astronomy",
+  author: "Stephen Hawking",
+  notes: "Black-Holes"
+  }];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
