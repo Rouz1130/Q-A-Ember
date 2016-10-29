96922b295e7463372d1cdb7cb1cf17a281fd6058
rouz majlessi <rmajlessi@gmail.com>
2016-10-29 15:41:23 -0700

    add hide and show topic functionality in question tile file

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  notesShowing: false,
+  actions: {
+    showNotes: function() {
+      this.set('notesShowing', true);
+    },
+    noNotes: function() {
+      this.set('notesShowing', false);
+    }
+  }
 });
